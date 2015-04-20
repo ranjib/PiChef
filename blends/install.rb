f3f63797af45122947b2ed3154df4f38d320d31e
Ranjib Dey <ranjib@pagerduty.com>
2015-04-20 00:56:03 -0700

    add installer script

---
+++ blends/install.rb
@@ -0,0 +1,9 @@
+ssh_task 'sudo apt-get update -y'
+ssh_task 'sudo apt-get install -y wget'
+
+ssh_task 'download chef' do
+ execute 'wget -c https://github.com/ranjib/PiChef/releases/download/0.0.1/chef-12.2.1.deb'
+end
+
+ssh_task 'sudo dpkg -i chef-12.2.1.deb'
+ssh_task 'rm chef-12.2.1.deb'
