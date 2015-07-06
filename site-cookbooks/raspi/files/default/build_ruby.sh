528e9bd1485f104d0b7d6edec8d91c404fbc54c3
Ranjib Dey <ranjib@pagerduty.com>
2015-07-05 18:25:26 -0700

    change ruby debian name

--- site-cookbooks/raspi/files/default/build_ruby.sh
+++ site-cookbooks/raspi/files/default/build_ruby.sh
@@ -7,5 +7,5 @@ cd ruby-2.2.2
 bash ./configure --enable-shared --disable-install-doc --disable-install-rdoc --disable-install-capi --prefix=/opt/rubies/2.2.2
 make
 make install
-/opt/chef/bin/fpm -s dir -t deb -n ruby -v 2.2.2 /opt/rubies/2.2.2
+/opt/chef/bin/fpm -s dir -t deb -n pi-ruby -v 2.2.2 /opt/rubies/2.2.2
 rm -rf /opt/rubies/2.2.2
