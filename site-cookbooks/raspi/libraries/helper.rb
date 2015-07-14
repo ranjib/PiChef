ee003c13125f5abff53cfa46b3674e9838964ecc
Ranjib Dey <ranjib@linux.com>
2015-07-14 00:07:50 -0700

    use correct array construct

--- site-cookbooks/raspi/libraries/helper.rb
+++ site-cookbooks/raspi/libraries/helper.rb
@@ -1,6 +1,6 @@
 module Raspi
   extend self
   def default_boot_options
-    %W(hdmi_force_hotplug=1 dtparam=i2c1=on dtparam=i2c_arm=on)
+    %w(hdmi_force_hotplug=1 dtparam=i2c1=on dtparam=i2c_arm=on)
   end
 end
