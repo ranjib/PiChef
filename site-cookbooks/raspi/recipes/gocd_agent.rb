2101e8b1d1302ff810db76bd0f4f2bed7f239cfd
Ranjib Dey <ranjib@linux.com>
2015-09-06 10:43:07 -0700

    nodejs recipe for gocd agent, for building grafana

--- site-cookbooks/raspi/recipes/gocd_agent.rb
+++ site-cookbooks/raspi/recipes/gocd_agent.rb
@@ -1,5 +1,15 @@
-user 'goatos'
+user 'goatos' do
+  home '/var/lib/go-agent'
+  shell '/bin/bash'
+end
+
+execute '/opt/chef/embedded/bin/gem install fpm --no-ri --no-rdoc' do
+  command '/opt/chef/embedded/bin/gem install fpm --no-ri --no-rdoc'
+  not_if'/opt/chef/embedded/bin/gem spec fpm'
+end
+
 node.default['go_cd']['java_home']= '/usr/lib/jvm/java-7-oracle'
 include_recipe 'raspi::java'
 include_recipe 'raspi::golang'
+include_recipe 'raspi::nodejs'
 include_recipe 'goatos::agent'
